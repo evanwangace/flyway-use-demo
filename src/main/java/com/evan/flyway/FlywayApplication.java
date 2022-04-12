4576fd9a27e307e9ad649531851e6f2b2867afe6
evan wang <wangmingmis@163.com>
2022-04-12 20:36:15 +0800

    feat:使用flyway测试数据库版本控制

---
+++ src/main/java/com/evan/flyway/FlywayApplication.java
@@ -0,0 +1,15 @@
+package com.evan.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author evan
+ * @date 2022-04-07
+ */
+@SpringBootApplication
+public class FlywayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayApplication.class, args);
+    }
+}
---
+++ src/main/resources/db/migration/2.1.7/V2__add_user.sql
@@ -0,0 +1,1 @@
+insert into `user`(user_name,age) values('lisi',33);
